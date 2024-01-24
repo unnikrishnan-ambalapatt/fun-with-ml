a89898996ed63c59a98aa848df51b1f108d2122f
Unnikrishnan Ambalapatt <49778290+unnikrishnan-ambalapatt@users.noreply.github.com>
2024-01-23 18:04:37 -0800

    Linear regression with scikit-learn

---
+++ linear_regression_with_scikit_learn.py
@@ -0,0 +1,32 @@
+import numpy as np
+import pandas as pd
+import sklearn.model_selection
+from sklearn import linear_model
+import pickle
+
+data = pd.read_csv("student-mat.csv", sep=";")
+data = data[["G1", "G2", "G3", "studytime", "failures", "absences"]]
+predict = "G3"
+X = np.array(data.drop(labels=[predict], axis=1))
+y = np.array(data[predict])
+X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.1)
+
+linear_modl = linear_model.LinearRegression()
+linear_modl.fit(X_train, y_train)
+accuracy = linear_modl.score(X_test, y_test)
+print('Accuracy: ' + str(accuracy))
+print('Coefficient: ' + str(linear_modl.coef_))
+print('Intercept: ' + str(linear_modl.intercept_))
+
+predictions = linear_modl.predict(X_test)
+for x in range(len(predictions)):
+    print(predictions[x], X_test[x], y_test[x])
+
+with open('studentmodel.pickle', 'wb') as f:
+    pickle.dump(linear_modl, f)
+
+pickle_input = open('studentmodel.pickle', 'rb')
+model_from_pickle = pickle.load(pickle_input)
+predictions_from_pickle = model_from_pickle.predict(X_test)
+for x in range(len(predictions_from_pickle)):
+    print(predictions_from_pickle[x], X_test[x], y_test[x])
