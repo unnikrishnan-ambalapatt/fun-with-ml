fff0f53c1655cb52840774dd21e57dc79c642e3d
Unnikrishnan Ambalapatt <49778290+unnikrishnan-ambalapatt@users.noreply.github.com>
2024-03-01 19:34:56 -0800

    K-nearest neighbors - Comments and clean-up

--- knn/knn.py
+++ knn/knn.py
@@ -1,12 +1,12 @@
-from sklearn.utils import shuffle
 from sklearn.neighbors import KNeighborsClassifier
 import pandas as pd
-import numpy as np
-from sklearn import linear_model, preprocessing, model_selection
+from sklearn import preprocessing, model_selection
 
+# Read CSV file
 data_frame = pd.read_csv("car.data")
 print(data_frame.head())
 
+# Convert strings to numbers using preprocessing
 le = preprocessing.LabelEncoder()
 buying = le.fit_transform(list(data_frame["buying"]))
 maint = le.fit_transform(list(data_frame["maint"]))
@@ -18,14 +18,20 @@ cls = le.fit_transform(list(data_frame["class"]))
 
 predict = "class"
 
+# Set input and output
 X = list(zip(buying, maint, door, persons, leg_boot, safety))
 y = list(cls)
 
+# Split input and output into training and testing sets
 x_train, x_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.1)
 
+# Define the algorithm for the model
 model = KNeighborsClassifier(n_neighbors=9)
 
+# Train the model
 model.fit(x_train, y_train)
+
+# Check accuracy of the model
 acc = model.score(x_test, y_test)
 
 predicted = model.predict(x_test)
