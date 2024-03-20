729b3d7fba3aea24f98183186faf2a955ef54959
Unnikrishnan Ambalapatt <49778290+unnikrishnan-ambalapatt@users.noreply.github.com>
2024-03-19 22:44:14 -0700

    Basic chains in langchain

--- langchain-fun/langchain_basics.py
+++ langchain-fun/langchain_basics.py
@@ -1,13 +1,22 @@
+from langchain_core.prompts import PromptTemplate
+from langchain.chains import LLMChain
 from openai import OpenAI
 
 client = OpenAI()
 
 completion = client.chat.completions.create(
-  model="davinci-002",
-  messages=[
-    {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
-  ]
+    model="davinci-002",
+    messages=[
+        {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
+    ]
 )
 print(completion.choices)
 print("============================")
 print(completion.choices[0].message)
+
+prompt_template_name = PromptTemplate(
+    input_variables=['cuisine'],
+    template="I want to open a restaurant for {cuisine} food. Suggest a fancy name for this."
+)
+
+name_chain = LLMChain(llm=client, prompt=prompt_template_name, output_key="restaurant_name")
