ecb59711d00c01afbab2cf89949d49c0c76e11d8
Unnikrishnan Ambalapatt <49778290+unnikrishnan-ambalapatt@users.noreply.github.com>
2024-03-21 18:08:09 -0700

    More Streamlit elements

--- streamlit-fun/streamlit_basics.py
+++ streamlit-fun/streamlit_basics.py
@@ -10,3 +10,10 @@ with st.sidebar:
     st.subheader("Left sidebar")
     pdf_docs = st.file_uploader(
         "This is a file uploader", accept_multiple_files=True)
+st.page_link(label='Google', page='https://www.google.com')
+st.write('Some random text for the page')
+st.checkbox('Check box')
+st.button('Button')
+st.caption('Interesting caption')
+st.video('https://www3.cde.ca.gov/download/rod/big_buck_bunny.mp4')
+st.color_picker(label='Choose a color')
